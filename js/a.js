09bfac16a032fb7c1284c7fe5fe1a2b3a2bf41a5
Amoswish <1572912653@qq.com>
2017-11-19 01:46:04 +0800

    erase可以实现，但是有bug明天继续改正

--- js/a.js
+++ js/a.js
@@ -6,6 +6,7 @@ function graph(){
     finalY:0,
     widthofdraw:10,
     color:"",
+    name:0,
   }
   o.setStart = function(_startX,_startY){
     o.startX = _startX
@@ -16,6 +17,9 @@ function graph(){
     o.finalX = _finalX
     o.finalY = _finalY
 
+  }
+  o.setName = function(_name){
+    o.name = _name
   }
   o.setWidthofdraw = function(_widthofdraw){
     o.widthofdraw = _widthofdraw
@@ -29,6 +33,33 @@ function graph(){
     var width = myCanvas.width
     var height = myCanvas.height
     context.clearRect(0,0,width,height);
+  }
+  o.delete = function(x,y,deletePool){
+    var tempname = -1
+    //传入当前摁下的x,y，并且找到摁下的对象名
+    //console.log(deletePool);
+    for (var i = 0; i < deletePool.length; i++) {
+      for(var m = 0; m < deletePool[i].length;m++){
+        for(var j = -10;j<10;j++){
+          for(var k = -10; k<10;k++){
+            //console.log("asd");
+            var tempx = x+j
+            var tempy = y+k
+            //console.log(deletePool);
+            //console.log(tempx+"bcd"+tempy);
+            //console.log(deletePool[i].nowx +"dsad"+deletePool[i].nowy);
+            if(tempx == deletePool[i][m].nowx && tempy == deletePool[i][m].nowy){
+              //console.log(deletePool[i][m]);
+              tempname = deletePool[i][m].name
+              //return (deletePool[i][m].name)
+              break
+            }
+          }
+        }
+      }
+    }
+    console.log(tempname);
+    return tempname
   }
   return o
 }
@@ -44,63 +75,96 @@ function circle(){
     context.stroke()
     context.closePath()
     //context.strokeRect(0, 0, 100, 100)
+    //o.name = _name
   }
-  o.redraw = function(){
-    var trace = new Array()
+  o.redraw = function(_name){
+     var trace = new Array()
     var staX = this.startX
     var staY = this.startY
-    var finX = this.finalY
+    var finX = this.finalX
     var finY = this.finalY
-    var dir=[[0,1],[0,-1],[1,0],[-1,0]]
-    //var map[finX-staX][finY-staY]
-    var map=new Array(); //定义一维数组
-    for(i=1;i<=finX-staX;i++)
-    {
-        map[i]=new Array(); //将每一个子元素又定义为数组
-        for(n=0;n<finY-staY;n++){
-            map[i][n]=2; //此时aa[i][n]可以看作是一个二级数组
-            console.log(map[i][n]);
+    // var dir=[[0,1],[0,-1],[1,0],[-1,0]]
+    // //var map[finX-staX][finY-staY]
+    // var map=new Array(); //定义一维数组
+    // //console.log(staX+"sa"+fin);
+    // for(i=1;i<=Math.abs(finX-staX);i++)
+    // {
+    //     map[i]=new Array(); //将每一个子元素又定义为数组
+    //     for(n=0;n<Math.abs(finY-staY);n++){
+    //         map[i][n]=2 //此时aa[i][n]可以看作是一个二级数组
+    //         //console.log(i+"s"+n);
+    //     }
+    // }
+    // function CheckEdge(x,y){
+    //   //console.log(x+"adad"+y);
+    // //  console.log(staX+"sasa"+staY);
+    //   if((x-staX)>0&&(y-staY>=0)){
+    //     if(map[x-staX][y-staY]!=3 && x>=staX && x<=finX && y>=staY && y<=finY){
+    //     //if(x>=staX && x<=finX && y>=staY && y<=finY){
+    //       return 1
+    //     }
+    //     else {
+    //       return 0
+    //     }
+    // }
+    // }
+    // function dfs(x,y,context){
+    //   console.log(x+"s"+y);
+    //   if (x == finX && y == finY) {
+    //     return
+    //   }
+    //     if((x-staX)>0&&(y-staY>0)){
+    //       map[x-staX][y-staY] = 3
+    //     }
+    //     if(context.isPointInStroke(x,y)){
+    //       var temp = {
+    //         nowx:x,
+    //         nowy:y,
+    //         name:1,
+    //       }
+    //       //console.log(x+"ss"+y);
+    //       //console.log(temp)
+    //       trace.push(temp)
+    //       return
+    //     }
+    //     for(var i = 0; i < 4;i++){
+    //       if(CheckEdge(x+dir[i][0],y+dir[i][1])){ // 按照规则生成下一个节点
+    //         dfs(x+dir[i][0],y+dir[i][1],context)
+    //       }
+    //     }
+    //     return
+    // }
+    function tempcheck(context){
+      for(var i = staX;i<finX ;i++){
+        for (var j = staY;j<finY;j++){
+          if(context.isPointInStroke(i,j)){
+            var temp = {
+              nowx:i,
+              nowy:j,
+              name:_name,
+            }
+            //console.log(x+"ss"+y);
+            //console.log(temp)
+            trace.push(temp)
         }
-    }
-    function CheckEdge(x,y){
-      //if(map[x][y]!=3 && x>=staX && x<=finX && y>=staY && y<=finY){
-      if(x>=staX && x<=finX && y>=staY && y<=finY){
-        return 1
       }
-      else {
-        return 0
       }
-    }
-    function dfs(x,y){
-        //map[x-staX][y-staY] = 3
-        if(context.isPointInStroke(x,y)){
-          var temp = {
-            nowx:x,
-            nowy:y,
-            name:1,
-          }
-          console.log(x+"ss"+y);
-          console.log(temp)
-          trace.push(temp)
-          return
-        }
-        for(var i = 0; i < 4;i++){
-          if(CheckEdge(x+dir[i][0],y+dir[i][1])){ // 按照规则生成下一个节点
-            dfs(x+dir[i][0],y+dir[i][1])
-          }
-        }
-        return
     }
     var myCanvas = document.getElementById("myCanvas")
     var context = myCanvas.getContext('2d')
     // context.strokeRect(this.startX, this.startY, this.finalX-this.startX, this.finalY-this.startY)
-    console.log("begin");
+    //console.log("begin");
     context.beginPath()
     context.arc(this.startX+(this.finalX-this.startX)/2,this.startY+(this.finalY-this.startY)/2,Math.abs(this.finalX-this.startX)/2,0,Math.PI * 2,true)
     //context.stroke()
-    console.log("begin1");
+    //console.log("begin1");
+    //console.log(context.isPointInPath(this.startX+20,this.startY+20))
+    //console.log("begin2");
+    //dfs(staX,staY,context)
+    tempcheck(context)
     context.closePath()
-    dfs(staX,staY)
+
+    //console.log(trace);
     return trace
   }
   return o
